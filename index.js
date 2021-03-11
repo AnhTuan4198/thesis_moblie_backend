382022af1392c90bba832518154e7815e321536d
AnhTuan4198 <whitejunh@gmail.com>
2021-03-11 21:32:36 +0700

    Add custom env

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const config = require('config');
 const PORT = process.env.PORT || 3030;
 
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/routes')(app);
 
+
+let key = config.get("privateKey");
+console.log(key);
 app.listen( PORT ,()=>{
     console.log("listenning on port " +PORT)
 })
