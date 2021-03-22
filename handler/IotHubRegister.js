e70229221069dbf0a152bad431eb642112389e56
AnhTuan4198 <whitejunh@gmail.com>
2021-03-22 15:07:40 +0700

    add device to iothub

---
+++ handler/IotHubRegister.js
@@ -0,0 +1,80 @@
+const ProvisioningTransport = require("azure-iot-provisioning-device-mqtt").Mqtt;
+
+//Provisioning Service  Client
+const ProvisioningServiceClient = require("azure-iot-provisioning-service").ProvisioningServiceClient;
+
+//Provisioning Service connection string 
+const ProvisionServiceConnectionString = 'HostName=thesisDPS.azure-devices-provisioning.net;SharedAccessKeyName=provisioningserviceowner;SharedAccessKey=/PgfdWwZhwNyHhLepsTcwTCh/vC7XcurMomHJRlmF6w='
+
+// Provisioning Device Client
+const SymmetricKeySecurityClient = require("azure-iot-security-symmetric-key").SymmetricKeySecurityClient;
+const ProvisioningDeviceClient = require("azure-iot-provisioning-device").ProvisioningDeviceClient;
+
+
+//config variable
+const provisioningHost = "thesisDPS.azure-devices-provisioning.net";
+const idScope = "0ne0025B427";
+
+//auto-gen variable
+const shortId = require("shortid");
+
+exports.enrollmentRegister = async function (req,res,next) {
+    try {
+        const provisionService = await ProvisioningServiceClient.fromConnectionString(ProvisionServiceConnectionString);
+        
+        let registrationId = shortId.generate();
+        const enrollment = {
+            registrationId,
+            deviceID:registrationId,
+            attestation: {
+                type: 'symmetricKey'
+            },
+            provisioningStatus: 'enabled',
+            allocationPolicy: 'geoLatency',
+        };
+
+        let result = await provisionService.createOrUpdateIndividualEnrollment(enrollment);
+        // return res.status(200).json({
+        //     result:result.responseBody  
+        // })
+        res.locals = result.responseBody;
+        return next();
+    } catch (error) {
+       return next(error.responseBody) 
+    }
+
+}
+
+exports.deviceRegister = async function(req,res,next ){
+    try {
+        const payload = res.locals;
+        const registrationId = payload.registrationId;
+        const symmetricKey = payload.attestation.symmetricKey.primaryKey;
+
+        const provisioningSecurityClient = new SymmetricKeySecurityClient(
+            registrationId,
+            symmetricKey
+        )
+
+        const provisioningClient = await ProvisioningDeviceClient.create(
+            provisioningHost,
+            idScope,
+            new ProvisioningTransport(),
+            provisioningSecurityClient
+        )
+
+        const response = await provisioningClient.register();
+        const deviceConnectionString = `HostName=${response.assignedHub};DeviceId=${response.deviceId};SharedAccessKey=${symmetricKey}`
+        
+        return res.status(200).json({
+            connectionString:deviceConnectionString
+        })
+    } catch (error) {
+        console.log(error)
+        return next(error.result)
+    }
+}
+
+
+// exports.enrollmentRegister();
+// exports.deviceRegister()
--- route/Module.js
+++ route/Module.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const _ = require("lodash");
+const { enrollmentRegister, deviceRegister } = require("../handler/IotHubRegister");
 const { addModule, getAllModules} = require("../handler/module");
 
 router.post('/register', addModule);
 
 router.get('/', getAllModules);
+
+router.get('/iot-hub-registration',enrollmentRegister,deviceRegister)
 
 module.exports = router;
