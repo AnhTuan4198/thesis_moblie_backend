8691a206a7141fa2dd8e104b1c3a1b7d220f3224
AnhTuan4198 <whitejunh@gmail.com>
2021-04-15 11:24:05 +0700

    fixed booking route

--- route/Mobile_booking.js
+++ route/Mobile_booking.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const {booking} = require("../handler/booking/mobile_booking")
-router.post("/mobile_booking",booking)
+router.post("/mobile",booking)
 
 module.exports = router;
